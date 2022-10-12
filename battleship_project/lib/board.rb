64c9e651ee30a7efc4861a30d6a70606837ecf4b
Morgan Rector <mmrector@yahoo.com>
2022-10-11 17:14:37 -0700

    part1

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,21 @@
 class Board
-  
+    attr_reader :size
+  def initialize(n)
+    @grid = Array.new(n){Array.new(n){|ele| ele = :N}} 
+    @size = n * n
+  end 
+
+
+  def [](arr)
+    @grid[arr[0]][arr[1]]
+  end
+
+  def []=(position,value)
+    @grid[position[0]][position[1]] = value
+  end
+
+  def num_ships
+    @grid.flatten.count(:S)
+  end 
+
 end
